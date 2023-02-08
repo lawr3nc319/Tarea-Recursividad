5ecb1c157ed9ae12349d58658ddc65dafcc889b6
harguedas <123706487+harguedas@users.noreply.github.com>
2023-02-07 22:23:51 -0600

    Main del ejercicio 2

--- TareaRecursividad/src/main/java/sc202/grupo4/tarearecursividad/Ejercicio2.java
+++ TareaRecursividad/src/main/java/sc202/grupo4/tarearecursividad/Ejercicio2.java
@@ -6,8 +6,22 @@ package sc202.grupo4.tarearecursividad;
 
 /**
  *
- * @author lawwb
  */
 public class Ejercicio2 {
-    
+    package practicab;
+import java.util.Scanner;
+import static practicab.ClaseB.Total; public class PracticaB {     
+    public static void main(String[] args) {
+        int numero;
+    // obtener la entrada del usuario
+    System.out.println("Ingrese un número: ");
+    Scanner input = new Scanner(System.in);
+    //convertir cadena en un entero
+    numero = input.nextInt();
+    //llamar al metodo
+    int total = Total(numero);
+    System.out.println("Total de la Suma = " + total);
+    //cerrar la instancia del escáner
+     input.close();
+    }
 }

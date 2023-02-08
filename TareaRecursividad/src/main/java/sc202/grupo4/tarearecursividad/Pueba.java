4a5ba73cc1b113c63a08253fa43f2eefe0b645d0
lawr3nc319 <laww.bl19@gmail.com>
2023-02-07 21:52:56 -0600

    Create Pueba.java

---
+++ TareaRecursividad/src/main/java/sc202/grupo4/tarearecursividad/Pueba.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sc202.grupo4.tarearecursividad;
+
+/**
+ *
+ * @author lawwb
+ */
+public class Pueba {
+    
+}
